034cc19a7dc75642d2afa69f7a3e0d4b213a0132
Kanaka Meier <kanakameier@gmail.com>
2024-06-02 16:01:34 -0400

    fixed import error

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import TicTacToe from './pages/TicTacToePage'
-import PongGame from './page/PongGamePage'
+import PongGame from './pages/PongGamePage'
 
 function App(){
   return(
