13223174ee9ac14cbcf15eb3e374af711b6a8153
Kanaka Meier <kanakameier@gmail.com>
2024-06-03 17:20:23 -0400

    updated game design and layout

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import TicTacToe from './pages/TicTacToePage'
 import RPSJokerAce from './pages/RPSJokerAcePage';
+import PongGame from './pages/PongGamePage';
 
 function App(){
   return(
@@ -10,6 +11,7 @@ function App(){
         <Switch>
           <Route path = "/tic-tac-toe" exact component = {TicTacToe} />
           <Route path = "/RPSJokerAce" exact component = {RPSJokerAce} />
+          <Route path = "/PongGame" exact component = {PongGame} />
         </Switch>
       </div>
     </Router>
